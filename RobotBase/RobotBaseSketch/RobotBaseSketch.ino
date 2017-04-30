48aa49a0cd4836feabe2495166cef74ccbb679ae
sundinc <christiansundin@gmail.com>
2017-04-30 14:49:35 +0200

    fixed folder structure in repo

--- RobotBase/RobotBaseSketch/RobotBaseSketch.ino
+++ RobotBase/RobotBaseSketch/RobotBaseSketch.ino
@@ -1,20 +1,59 @@
+#include "FixedPointArithmetic.h"
 /*
  * RobotBaseSketch.ino
  *
- * Created: 4/11/2017 8:32:53 PM
+ * Created: 4/11/2017 8:36:21 PM
  * Author: Sundin
  */ 
+#include "Arduino.h"
+#include "sensorAccGyro.h"
+
+float x, y;
+Fix16 a, b;
+sensorAccGyro Sensor = sensorAccGyro(1,2,3,4,5, 3,4);
 
 void setup()
 {
-
+	Serial.begin(115200);
 	  /* add setup code here, setup code runs once when the processor starts */
 
 }
 
 void loop()
 {
+	x = 8.0;  //11 clock cycles
+	y = 2.5;  //12 clock cycles
+	//fixed point
+	unsigned long t1 = 0, t2 = 0;
+	float res = 0.0;
+	for(int i = 0; i<10; i++)
+	{
+		t1 = micros();
+		res = x * y;
+		t2 = micros();
+		Serial.print("Res: ");
+		Serial.print(res);
+		Serial.print(" t= ");
+		Serial.println(t2-t1);
+		delay(100);
+	}
+	for(int i = 0; i<10; i++)
+	{
 
+		a = 
+		b = 
+		res =  a*b;
+		t2 = micros();
+		Serial.print("Res: ");
+		Serial.print(res);
+		Serial.print(" t= ");
+		Serial.println(t2-t1);
+		delay(100);
+	}
+	while(1);
 	  /* add main program code here, this code starts again each time it ends */
 
 }
+
+
+
